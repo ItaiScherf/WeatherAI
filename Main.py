8a08490e6f2cae95118fb4dec15d05d189a055cf
ItaiScherf <63411138+ItaiScherf@users.noreply.github.com>
2021-06-30 00:54:28 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,6 +3,12 @@ from DL4 import *
 import matplotlib.pyplot as plt
 import numpy as np
 
+import tkinter
+from tkinter import *
+from tkinter import filedialog
+from tkinter.filedialog import askopenfilename
+from tkinter import messagebox
+from tkinter.messagebox import *
 
 def EnterDate():
     print("Please enter the day:")
@@ -14,31 +20,51 @@ def EnterDate():
     return d1, m1, y1
 
 
-# X = np.expand_dims(Get_X(24,5,2021), axis=0).transpose()
-# Y = np.expand_dims(Get_Y(25,5,2021), axis=0).transpose()
-# X = np.array([Get_X(24,1,2021),Get_X(23,1,2021),Get_X(22,1,2021),Get_X(21,1,2021),Get_X(20,1,2021),Get_X(19,1,2021),Get_X(18,1,2021),Get_X(17,1,2021),Get_X(16,1,2021),Get_X(15,1,2021),Get_X(14,1,2021),Get_X(13,1,2021),Get_X(12,1,2021),Get_X(11,1,2021),Get_X(10,1,2021),Get_X(9,1,2021),Get_X(8,1,2021)]).transpose()
-# Y = np.array([Get_Y(25,1,2021),Get_Y(24,1,2021),Get_Y(23,1,2021),Get_Y(22,1,2021),Get_Y(21,1,2021),Get_Y(20,1,2021),Get_Y(19,1,2021),Get_Y(18,1,2021),Get_Y(17,1,2021),Get_Y(16,1,2021),Get_Y(15,1,2021),Get_Y(14,1,2021),Get_Y(13,1,2021),Get_Y(12,1,2021),Get_Y(11,1,2021),Get_Y(10,1,2021),Get_Y(9,1,2021)]).transpose()
-# print(X.shape)
-# print(Y.shape)
-
 # model.train(X,Y,500)
 # model.predict(np.expand_dims(Get_X(26,1,2021), axis=0).transpose())
-# model.confusion_matrix(Get_X(26,1,2021),Get_Y(27,1,2021))
-# np.expand_dims()
 
+def center_window(w=300, h=200):
+    # get screen width and height
+    ws = window.winfo_screenwidth()
+    hs = window.winfo_screenheight()
+    # calculate position x, y
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    window.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-cur_path = "D:\אולטרה תיקייה\הנדסת תוכנה\data"
+def setTrain():
+    global train_test
+    train_test="train"
+
+def setTest():
+    global train_test
+    train_test="test"
+
+window = Tk()
+window.geometry("680x350")
+center_window(680, 350)
+window.title("Train or Test")
+Label(window, text ="Please choose train/test...", font=('times', 24)).pack()
+btn1=tkinter.Button(window, width=15, height=2, text="Training",font=('times', 24),command = lambda:[setTrain(),window.destroy()]).place(x=50,y=100)
+btn2=tkinter.Button(window, width=15, height=2, text="Testing",font=('times', 24),command =lambda:[setTest(),window.destroy()]).place(x=350,y=100)
+label1=Label(window, text ="Testing will take pretrained parameters...",anchor = 'center', font=('times', 24)).place(x=50,y=250)
+window.mainloop()
+
+cur_path = "C:\\הנדסת תוכנה\\WeatherAI\\Data"
 # train_path = os.path.join(cur_path, 'data\\train')
 train_list = ["storm awaits!", "rainy", "strong winds", "very cold", "cold", "very hot", "hot", "lovely day"]
 # trainDict = {i: train_list[i] for i in range(0, len(train_list))}
 # test_path = os.path.join(cur_path, 'data\\test')
 
-print("Will you test me or train me? ⍨")
-train_test = input()
 
 if (train_test == 'train'):
 
     X_train, Y_train, X_test, Y_test = Get_Data_from_to(1, 6, 2021, 15, 6, 2021)
+
+    print("X train shape:" + str(X_train.shape))
+    print("X test shape:" + str(X_test.shape))
+    print("Y train shape:" + str(Y_train.shape))
+    print("Y test shape:" + str(Y_test.shape))
 
     W_init1 = "Xaviar"
     W_init2 = "Xaviar"
@@ -57,12 +83,18 @@ else:
     W_init6 = cur_path + "\\Layer6.h5"
 
 model = DLModel("Weather Forcast")
-model.add(DLLayer("#1", 50, (X_train.shape[0],), "trim_tanh", W_init1, 0.01, "adaptive"))
-model.add(DLLayer("#2", 20, (50,), "trim_tanh", W_init2, 0.01, "adaptive"))
-model.add(DLLayer("#3", 20, (20,), "trim_tanh", W_init3, 0.01, "adaptive"))
-model.add(DLLayer("#4", 10, (20,), "trim_tanh", W_init4, 0.01, "adaptive"))
-model.add(DLLayer("#5", 10, (10,), "trim_tanh", W_init5, 0.01, "adaptive"))
-model.add(DLLayer("softmax", 8, (10,), "trim_softmax", W_init6, 0.01, "adaptive"))
+layer1 = DLLayer("#1", 100, (X_train.shape[0],), "trim_tanh", W_init1, 0.01, "adaptive")
+layer2 = DLLayer("#2", 50, (100,), "trim_tanh", W_init2, 0.01, "adaptive")
+layer3 = DLLayer("#3", 50, (50,), "trim_tanh", W_init3, 0.01, "adaptive")
+layer4 = DLLayer("#4", 50, (50,), "trim_tanh", W_init4, 0.01, "adaptive")
+layer5 = DLLayer("#5", 20, (50,), "trim_tanh", W_init5, 0.01, "adaptive")
+layer6 = DLLayer("softmax", 8, (20,), "trim_softmax", W_init6, 0.01, "adaptive")
+model.add(layer1)
+model.add(layer2)
+model.add(layer3)
+model.add(layer4)
+model.add(layer5)
+model.add(layer6)
 model.compile("categorical_cross_entropy")
 
 if (train_test == 'train'):
@@ -87,7 +119,12 @@ if (train_test == 'train'):
     print()
 
     print("Saving weights...")
-    model.save_weights(cur_path)
+    layer1.save_weights(cur_path, "layer1")
+    layer2.save_weights(cur_path, "layer2")
+    layer3.save_weights(cur_path, "layer3")
+    layer4.save_weights(cur_path, "layer4")
+    layer5.save_weights(cur_path, "layer5")
+    layer6.save_weights(cur_path, "layer6")
 
 if train_test == "test":
     d1, m1, y1 = EnterDate()
