3272dd354e5d3fb3c3e5ba6c78612980f0f16e6b
ItaiScherf <63411138+ItaiScherf@users.noreply.github.com>
2021-06-30 13:38:55 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,11 +12,11 @@ from tkinter.messagebox import *
 
 def EnterDate():
     print("Please enter the day:")
-    d1 = input()
+    d1 = int(input())
     print("Please enter the month:")
-    m1 = input()
+    m1 = int(input())
     print("Please enter the year:")
-    y1 = input()
+    y1 = int(input())
     return d1, m1, y1
 
 
@@ -50,16 +50,21 @@ btn2=tkinter.Button(window, width=15, height=2, text="Testing",font=('times', 24
 label1=Label(window, text ="Testing will take pretrained parameters...",anchor = 'center', font=('times', 24)).place(x=50,y=250)
 window.mainloop()
 
-cur_path = "C:\\הנדסת תוכנה\\WeatherAI\\Data"
+cur_path = r'C:\Users\Itai Scherf\PycharmProjects\pythonProject1\paramaters summer'
 # train_path = os.path.join(cur_path, 'data\\train')
 train_list = ["storm awaits!", "rainy", "strong winds", "very cold", "cold", "very hot", "hot", "lovely day"]
 # trainDict = {i: train_list[i] for i in range(0, len(train_list))}
 # test_path = os.path.join(cur_path, 'data\\test')
 
+# X_train, Y_train, X_test, Y_test = Get_Data_from_to(1, 6, 2021, 15, 6, 2021)
+with h5py.File(r'C:\Users\Itai Scherf\PycharmProjects\pythonProject1\Database\WeatherData.h5', 'r') as hf:
+    X_train = hf['x_train'][:]
+    Y_train = hf['labels_train'][:]
+    X_test = hf['x_test'][:]
+    Y_test = hf['labels_test'][:]
 
 if (train_test == 'train'):
 
-    X_train, Y_train, X_test, Y_test = Get_Data_from_to(1, 6, 2021, 15, 6, 2021)
 
     print("X train shape:" + str(X_train.shape))
     print("X test shape:" + str(X_test.shape))
@@ -72,32 +77,23 @@ if (train_test == 'train'):
     W_init4 = "Xaviar"
     W_init5 = "Xaviar"
     W_init6 = "Xaviar"
+    layer1 = DLLayer("#1", 100, (X_train.shape[0],), "trim_tanh", W_init1, 0.01, "adaptive")
+    layer2 = DLLayer("#2", 50, (100,), "trim_tanh", W_init2, 0.01, "adaptive")
+    layer3 = DLLayer("#3", 50, (50,), "trim_tanh", W_init3, 0.01, "adaptive")
+    layer4 = DLLayer("#4", 50, (50,), "trim_tanh", W_init4, 0.01, "adaptive")
+    layer5 = DLLayer("#5", 20, (50,), "trim_tanh", W_init5, 0.01, "adaptive")
+    layer6 = DLLayer("softmax", 8, (20,), "trim_softmax", "Xaviar", 0.01, "adaptive")
 
-else:
+    model = DLModel("Weather Forcast")
 
-    W_init1 = cur_path + "\\Layer1.h5"
-    W_init2 = cur_path + "\\Layer2.h5"
-    W_init3 = cur_path + "\\Layer3.h5"
-    W_init4 = cur_path + "\\Layer4.h5"
-    W_init5 = cur_path + "\\Layer5.h5"
-    W_init6 = cur_path + "\\Layer6.h5"
+    model.add(layer1)
+    model.add(layer2)
+    model.add(layer3)
+    model.add(layer4)
+    model.add(layer5)
+    model.add(layer6)
+    model.compile("categorical_cross_entropy")
 
-model = DLModel("Weather Forcast")
-layer1 = DLLayer("#1", 100, (X_train.shape[0],), "trim_tanh", W_init1, 0.01, "adaptive")
-layer2 = DLLayer("#2", 50, (100,), "trim_tanh", W_init2, 0.01, "adaptive")
-layer3 = DLLayer("#3", 50, (50,), "trim_tanh", W_init3, 0.01, "adaptive")
-layer4 = DLLayer("#4", 50, (50,), "trim_tanh", W_init4, 0.01, "adaptive")
-layer5 = DLLayer("#5", 20, (50,), "trim_tanh", W_init5, 0.01, "adaptive")
-layer6 = DLLayer("softmax", 8, (20,), "trim_softmax", W_init6, 0.01, "adaptive")
-model.add(layer1)
-model.add(layer2)
-model.add(layer3)
-model.add(layer4)
-model.add(layer5)
-model.add(layer6)
-model.compile("categorical_cross_entropy")
-
-if (train_test == 'train'):
     cost = model.train(X_train, Y_train, 500)
     plt.plot(np.squeeze(cost))
     plt.ylabel("cost")
@@ -119,18 +115,36 @@ if (train_test == 'train'):
     print()
 
     print("Saving weights...")
-    layer1.save_weights(cur_path, "layer1")
-    layer2.save_weights(cur_path, "layer2")
-    layer3.save_weights(cur_path, "layer3")
-    layer4.save_weights(cur_path, "layer4")
-    layer5.save_weights(cur_path, "layer5")
-    layer6.save_weights(cur_path, "layer6")
+    model.save_weights(r'C:\Users\Itai Scherf\PycharmProjects\pythonProject1\paramaters summer')
+else:
 
-if train_test == "test":
+    W_init1 = cur_path + "\Layer1.h5"
+    W_init2 = cur_path + "\Layer2.h5"
+    W_init3 = cur_path + "\Layer3.h5"
+    W_init4 = cur_path + "\Layer4.h5"
+    W_init5 = cur_path + "\Layer5.h5"
+
     d1, m1, y1 = EnterDate()
-    print ("please wait a moment 😃")
-    x_test = Get_X(d1, m1, y1)
-    predictions = model.predict(X_train)
+    print("please wait a moment 😃")
+    X = np.expand_dims(Get_X(d1, m1, y1), axis=0).transpose()
+
+    layer1 = DLLayer("#1", 100, (X.shape[0],), "trim_tanh", W_init1, 0.01, "adaptive")
+    layer2 = DLLayer("#2", 50, (100,), "trim_tanh", W_init2, 0.01, "adaptive")
+    layer3 = DLLayer("#3", 50, (50,), "trim_tanh", W_init3, 0.01, "adaptive")
+    layer4 = DLLayer("#4", 50, (50,), "trim_tanh", W_init4, 0.01, "adaptive")
+    layer5 = DLLayer("#5", 20, (50,), "trim_tanh", W_init5, 0.01, "adaptive")
+    layer6 = DLLayer("softmax", 8, (20,), "trim_softmax", "Xaviar", 0.01, "adaptive")
+
+    model = DLModel("Weather Forcast")
+    model.add(layer1)
+    model.add(layer2)
+    model.add(layer3)
+    model.add(layer4)
+    model.add(layer5)
+    model.add(layer6)
+    model.compile("categorical_cross_entropy")
+
+    predictions = model.predict(X)
     # ans = trainDict[DLModel.conv(predictions)]
     # root = Tk()
     # root.overrideredirect(1)
